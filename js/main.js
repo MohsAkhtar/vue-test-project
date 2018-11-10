355410f46b52cc3f0bb7ba7a7a2b30a15e6aede8
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-10 18:46:37 +0000

    Added input validation

---
+++ js/main.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#app',
+  data: {
+    state: true,
+    inputName: '',
+    names: [],
+    showError = false
+  },
+  methods: {
+    addNameToList() {
+      if (this.validate(this.inputName)) {
+        this.names.push(this.inputName);
+        this.inputName = '';
+        this.showError = false;
+      } else {
+        this.showError = true;
+      }
+    },
+    validate(value) {
+      if (value !== '') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+});
